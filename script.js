16b55d54901fa65312689045d8f59f9c5d550687
Jordan <JMCollins0@aol.com>
2022-03-01 11:36:30 -0500

    Modify player choice function to reflect CPU choice function.

--- script.js
+++ script.js
@@ -1,24 +1,56 @@
-
+let plrWins = 0;
+let cmpWins = 0;
 
 function getPlayerChoice(){
-    let playerChoice = prompt("Choose R for rock P for paper or S for scissors.").toLocaleUpperCase();
-    if (playerChoice.length > 1){
-        alert("Too many Characters detected please enter only one letter");
-        let playerChoice = prompt("Choose R for rock P for paper or S for scissors.").toLocaleUpperCase();
-    }
-    else{
-        return playerChoice;
-    }
-    
+    let Choice = "RPS"
+    let plrChoice = Choice[Number(prompt("Choose 0 for rock 1 for paper and 2 for scissors."))];
+    return plrChoice;
 }
 
 function getCPUChoice(){
     let ComputerChoice = "RPS"
     return ComputerChoice[Math.floor(Math.random()* 3)]; 
+    // return ComputerChoice[0];
+    
 }
 
+function chooseRoundWinner(player, computer){
+    if (player === computer){
+        console.log("Its A tie!")
+        plrWins++;
+        cmpWins++;
+        return;
+    }
+    //all computer win situations
+    if ((computer == "P" && player == "R") || (computer == "S" && player == "P") || (computer == "R" && player == "S")){
+        cmpWins++;
+        return;
+    }
+    // else if(computer == "S" && player == "P"){
+    //     cmpWins++;
+    // }
+    // else if (computer == "R" && player == "S"){
+    //     cmpWins++;
+    // }
+    else{
+        plrWins++;
+    }
 
+}
 
+function decideTheGame(){
+    let i = 0;
+    while (i < 5){
+    chooseRoundWinner(getPlayerChoice(), getCPUChoice());
+    console.log(`Player wins are ${plrWins} Computer wins are ${cmpWins}` );
+    i++;
+    }
+    if (plrWins > cmpWins){
+        console.log("You win!!!");
+    }
+    else{
+        console.log("The CPU Won!");
+    }
+}
 
-console.log(getCPUChoice());
-
+decideTheGame();
