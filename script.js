7de2bbea307791c294bb7353dd872c6bdd153d9b
Jordan <JMCollins0@aol.com>
2022-03-27 09:19:32 -0400

    add new UI elements for a scoreboard and add the updateScore function.

--- script.js
+++ script.js
@@ -2,60 +2,73 @@ const plrChoiceRock = document.getElementById("rock");
 const plrChoicePaper = document.getElementById("paper");
 const plrChoiceScissors = document.getElementById("scissors");
 const div = document.getElementById("playerChoiceReturn");
+const playerScore = document.getElementById("playerScore");
+const CPUScore = document.getElementById("CPUScore");
 
-let plrWins = 0;
+let plrWins = 0; // Will display to the player score/ cpu score divs as the game progresses.
 let cmpWins = 0;
 
-function getPlayerChoice(x){
-    let Choice = "RPS";
-    div.textContent = Choice[x];
-    return Choice[x]
+
+function updateScore(plrWins, cmpWins){
+    playerScore.textContent = `Your score is: ${plrWins}`
+    CPUScore.textContent = `Your score is: ${cmpWins}`
 }
 
-plrChoiceRock.addEventListener("click", function (){chooseRoundWinner(getPlayerChoice(0), getCPUChoice())})
-plrChoicePaper.addEventListener("click", function () {chooseRoundWinner(getPlayerChoice(1),getCPUChoice() )});
-plrChoiceScissors.addEventListener("click",function () {chooseRoundWinner(getPlayerChoice(2), getCPUChoice())});
-
-
-
-function getCPUChoice(){
-    let ComputerChoice = "RPS"
-    return ComputerChoice[Math.floor(Math.random()* 3)] 
+function getPlayerChoice(x) {
+  let Choice = "RPS";
+  div.textContent = Choice[x];
+  return Choice[x];
 }
 
+plrChoiceRock.addEventListener("click", function () {
+  chooseRoundWinner(getPlayerChoice(0), getCPUChoice());
+  updateScore(plrWins, cmpWins);
+});
+plrChoicePaper.addEventListener("click", function () {
+  chooseRoundWinner(getPlayerChoice(1), getCPUChoice());
+  updateScore(plrWins, cmpWins);
+});
+plrChoiceScissors.addEventListener("click", function () {
+  chooseRoundWinner(getPlayerChoice(2), getCPUChoice());
+  updateScore(plrWins, cmpWins)
+});
 
-function chooseRoundWinner(player, computer){
-    console.log(player);
-    console.log(computer);
+function getCPUChoice() {
+  let ComputerChoice = "RPS";
+  return ComputerChoice[Math.floor(Math.random() * 3)];
+}
+
+function chooseRoundWinner(player, computer) {
+  console.log(player);
+  console.log(computer);
+
+  if (player === computer) {
+    div.textContent = "Tied Round";
+    plrWins++;
+    cmpWins++;
     
-    if (player === computer){
-        div.textContent = "Tied Round"
-        plrWins++;
-        cmpWins++;
-    }
-    //all computer win situations
-    if ((computer == "P" && player == "R") || (computer == "S" && player == "P") || (computer == "R" && player == "S")){
-        cmpWins++;
-    }
-    else{
-        plrWins++;
-    }
+  }
+  //all computer win situations
+  if (
+    (computer == "P" && player == "R") ||
+    (computer == "S" && player == "P") ||
+    (computer == "R" && player == "S")
+  ) {
+    div.textContent = "CPU wins the round!";
+    cmpWins++;
+    
+  } else {
+    div.textContent = "You win the round!";
+    plrWins++;
+    
+  }
+}
 
-    if (plrWins === 3){
-        div.textContent = "You Win";
-        plrWins = 0;
-        cmpWins = 0;
-    }
-
-    if (cmpWins === 3){
-        div.textContent = "You Lose";
-        plrWins = 0;
-        cmpWins = 0;
-        }
-    }
-
-
-
+function resetGame() {
+  plrWins = 0;
+  cmpWins = 0;
+  updateScore(plrWins, cmpWins);
+}
 
 /* let i = 0;
 while (i < 5){
